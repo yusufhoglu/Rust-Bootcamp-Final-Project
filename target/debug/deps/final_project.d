be1218b553e8624d8276b7def83b2eddf0ea700f
yusuf <hocaogluyusuf35@gmail.com>
2023-07-25 11:19:33 +0300

    initial commit

---
+++ target/debug/deps/final_project.d
@@ -0,0 +1,5 @@
+C:\Users\hocao\OneDrive\Masaüstü\Yazılım\Rust\final\final_project\target\debug\deps\final_project.exe: src\main.rs
+
+C:\Users\hocao\OneDrive\Masaüstü\Yazılım\Rust\final\final_project\target\debug\deps\final_project.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/final_project.d
@@ -0,0 +1,1 @@
+C:\Users\hocao\OneDrive\Masaüstü\Yazılım\Rust\final\final_project\target\debug\final_project.exe: C:\Users\hocao\OneDrive\Masaüstü\Yazılım\Rust\final\final_project\src\main.rs
